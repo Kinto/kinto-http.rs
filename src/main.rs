1c1a2babb624ee6ae76274f7b9172bc4a5d5b55b
Rémy HUBSCHER <rhubscher@mozilla.com>
2017-02-05 16:11:49 +0100

    Pretty print the JSON

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,34 @@
 extern crate hyper;
+extern crate rustc_serialize;
 
-use std::io;
+use std::env;
+use std::io::Read;
+
 use hyper::client::Client;
 use hyper::header::Connection;
+use rustc_serialize::json;
+use rustc_serialize::json::Json;
+
 
 fn main() {
-    let client = Client::new();
-    let mut res = client.get("http://www.perdu.com/")
-        .header(Connection::close())
-        .send().unwrap();
+    // Prints each argument on a separate line
+    let args: Vec<String> = env::args().collect();
+    let server_url = &args[1];
 
-    println!("Response: {}", res.status);
-    println!("Headers:\n{}", res.headers);
-    io::copy(&mut res, &mut io::stdout()).unwrap();
+    let client = Client::new();
+    let mut res = client
+        .get(server_url)
+        .header(Connection::close())
+        .send()
+        .unwrap();
+
+    println!("HTTP/1.1 {}", res.status);
+    println!("{}", res.headers);
+
+    let mut body = String::new();
+    res.read_to_string(&mut body).unwrap();
+    let data = Json::from_str(&body).unwrap();
+
+
+    println!("{}", json::as_pretty_json(&data));
 }
