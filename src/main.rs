c22434a5e461cc59bf173e96c69eea2f36c77614
Gabriela Surita <gabsurita@gmail.com>
2017-02-07 01:03:14 -0200

    Add stupid sample app
    Just to make sure that it works.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+extern crate hyper;
+extern crate json;
+
+use hyper::header::{Authorization, Basic};
+
+
 extern crate kinto_http;
 
 
@@ -5,8 +11,21 @@ fn main() {
     // Let's get the URL from the CLI arguments
     let args: Vec<String> = std::env::args().collect();
     let server_url = &args[1];
+    let auth = Authorization(
+        Basic {
+            username: "a".to_owned(),
+            password: Some("a".to_owned()),
+        }
+    );
+    let client = kinto_http::Client::new(server_url.clone(), auth);
 
-    let client = kinto_http::Client::new(server_url.clone(), 80);
-    let info = client.server_info();
-    println!("{}", kinto_http::prettyfy(info));
+    let mut record = json::JsonValue::new_object();
+    record["id"] = "cachaca".into();
+
+    //let info = client.server_info();
+    let create = client.create_record("default".into(), "drinks".into(), record.into());
+    let drinks = client.get_records("default".into(), "drinks".into());
+
+    println!("{}", kinto_http::prettyfy(create.unwrap()));
+    println!("{}", kinto_http::prettyfy(drinks.unwrap()));
 }
